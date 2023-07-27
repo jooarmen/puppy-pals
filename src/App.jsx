1dbd1477764529aab8349cabd88a0f4ff7de4932
jooarmen <126363143+jooarmen@users.noreply.github.com>
2023-07-26 21:16:31 -0700

    data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
 
+import {puppyList} from './data.js'
 
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+console.log(puppies)
+  return (
+    <>
+      <div>
+     
+      </div>
+    </>
+  )
+}
 
-
-  
-<div>
-  
-</div>
-  
-
-
-
+export default App
